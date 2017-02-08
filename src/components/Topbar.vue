01c285b1a156b5ab434b40d78b858c163d594ecb
StarLikeRain <pengyaokang@qq.com>
2017-02-08 09:54:06 +0800

    push your one face...

---
+++ src/components/Topbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="topbar">
+    Resume
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Topbar'
+  }
+</script>
+
+<style>
+  #topbar{
+    background:#ffffff;
+    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+  }
+  #topbar>.wrapper{
+    min-width: 1024px;
+    max-width: 1440px;
+    margin: 0 auto;
+    height:64px;
+  }
+  #topbar>.wrapper{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 16px;
+  }
+  #topbar .logo{
+    font-size:24px;
+    color:#000000;
+  }
+</style>
