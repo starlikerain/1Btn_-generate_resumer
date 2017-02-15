f5e094fe5912d45f12cb8b407428d2b6399e888e
StarLikeRain <pengyaokang@qq.com>
2017-02-15 10:30:56 +0800

    resumer add frameworkd content

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,68 @@
 <template>
-  <div class=page>
-    <header>
-      <Topbar/>
-    </header>
-
-    <main>
-      <ResumeEditor/>
-      <ResumePreview/>
-    </main>
-  </div>
+    <div class=page>
+        <header>
+            <Topbar/>
+        </header>
+        <main>
+            <ResumeEditor/>
+            <ResumePreview/>
+        </main>
+    </div>
 </template>
 
 <script>
-  import 'normalize.css/normalize.css'
-  import './assets/reset.css'
+    import 'normalize.css/normalize.css'
+    import './assets/reset.css'
 
-  import Topbar from './components/Topbar'
-  import ResumeEditor from './components/ResumeEditor'
-  import ResumePreview from './components/ResumePreview'
+    import Topbar from './components/Topbar'
+    import ResumeEditor from './components/ResumeEditor'
+    import ResumePreview from './components/ResumePreview'
+    import icons from './assets/icons'
 
-  export default {
-    name: 'app',
-    data: function () {
-      return {
-        text: ''
-      }
-    },
-    components: {Topbar, ResumeEditor, ResumePreview}
-  }
+    export default {
+        name: 'app',
+        data () {
+            return {
+                text: '안녕하십니까'
+            }
+        },
+        components: {Topbar, ResumeEditor, ResumePreview},
+        created(){
+            document.body.insertAdjacentHTML('afterbegin', icons)
+        }
+    }
 </script>
 
-<style>
-  .page {
-    height: 100vh;
-    min-width: 1024px;
-    display: flex;
-    flex-direction: column;
-    background: #EAEBEC;
-  }
+<style rel="stylesheet/scss" lang="scss">
+    .page {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        background: #EAEBEC;
+        > main {
+            flex-grow: 1;
+        }
+        > main {
+            min-width: 1024px;
+            max-width: 1440px;
+            margin-top: 16px;
+            margin-bottom: 16px;
+            display: flex;
+            justify-content: space-between;
+            padding: 0 16px;
+            width: 100%;
+            align-self: center;
+        }
+    }
 
-  .page > main {
-    flex-grow: 1;
-  }
+    #resumeEditor {
+        width: 35%;
+        background: #444;
+    }
 
-  .page > main {
-    min-width: 1024px;
-    max-width: 1440px;
-    margin-top: 16px;
-    margin-bottom: 16px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 16px;
-    width: 100%;
-    align-self: center;
-  }
-
-  #resumeEditor {
-    width: 35%;
-    background: #444;
-  }
-
-  #resumePreview {
-    flex-grow: 1;
-    margin-left: 16px;
-    background: #777;
-  }
+    #resumePreview {
+        flex-grow: 1;
+        margin-left: 16px;
+        background: #777;
+    }
 </style>
--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -1,18 +1,152 @@
 <template>
-  <div id="resumeEditor">
-    I am resumeEditor
-  </div>
+    <div id="resumeEditor">
+        <nav>
+            <ol>
+                <li v-for="(item,index) in resume.config" :class="{active: item.field === selected}"
+                    @click="selected = item.field">
+                    <svg class="icon" :class="item.field === selected? 'qunimei': 'wocao'">
+                        <use :xlink:href="`#icon-${item.icon}`"></use>
+                    </svg>
+                </li>
+            </ol>
+        </nav>
+        <ol class="panels">
+            <li v-for="item in resume.config" v-show="item.field === selected">
+                <!--「需要判断数据是Array或Object」-->
+                <div v-if="resume[item.field] instanceof Array">
+                    <div class="subitem" v-for="subitem in resume[item.field]">
+                        <div class="resumeField" v-for="(value,key) in subitem">
+                            <label> {{key}} </label>
+                            <input type="text" :value="value">
+                        </div>
+                        <hr>
+                    </div>
+                </div>
+                <!--{{resume[item.field]}}-->
+                <!--「 { "name": "", "city": "", "title": "" } 」-->
+
+                <!--「 v-else」-->
+                <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
+                    <label> {{key}} </label>
+                    <input type="text" v-model="resume[item.field][key]">
+                </div>
+            </li>
+        </ol>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'ResumeEditor'
-  }
+    export default {
+        name: 'ResumeEditor',
+        data() {
+            return {
+                selected: 'profile',
+                resume: {
+                    config: [
+                        {field: 'profile', icon: 'id'},
+                        {field: 'work history', icon: 'work'},
+                        {field: 'education', icon: 'book'},
+                        {field: 'projects', icon: 'heart'},
+                        {field: 'awards', icon: 'cup'},
+                        {field: 'contacts', icon: 'phone'},
+                    ],
+                    profile: {
+                        name: '',
+                        city: '',
+                        title: ''
+                    },
+                    'work history': [
+                        {company: 'AL', content: '我的第二份工作是'},
+                        {company: 'TX', content: '我的第一份工作是'}
+                    ],
+                    education: [
+                        {school: 'AL', content: '文字'},
+                        {school: 'TX', content: '文字'}
+                    ],
+                    projects: [
+                        {school: 'project A', content: '文字'},
+                        {school: 'project B', content: '文字'}
+                    ],
+                    awards: [
+                        {school: 'awards A', content: '文字'},
+                        {school: 'awards B', content: '文字'}
+                    ],
+                    contacts: [
+                        {school: 'phone', content: '12342341514'},
+                        {school: 'QQ', content: '23141234'}
+                    ],
+                }
+            }
+        }
+    }
 </script>
 
-<style>
-  #resumeEditor{
-    background:#ffffff;
-    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
-  }
+<style rel="stylesheet/scss" lang="scss" scoped>
+    #resumeEditor {
+        background: #ffffff;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+        display: flex;
+        flex-direction: row;
+        overflow: auto;
+        > nav {
+            min-width: 80px;
+            background: black;
+            color: white;
+            > ol {
+                > li {
+                    height: 48px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    margin-top: 16px;
+                    margin-bottom: 16px;
+                    &.active {
+                        background: white;
+                    }
+                }
+            }
+        }
+        > .panels {
+            flex-grow: 1;
+            > li {
+                padding: 24px;
+            }
+        }
+        svg.icon {
+            width: 24px;
+            height: 24px;
+        }
+    }
+
+    .wocao {
+        fill: white;
+    }
+
+    .qunimei {
+        fill: black;
+    }
+
+    ol {
+        list-style: none;
+    }
+
+    .resumeField {
+        > label {
+            display: block;
+        }
+        input[type=text] {
+            margin: 16px 0;
+            border: 1px solid #ddd;
+            box-shadow: inset 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+            width: 100%;
+            height: 40px;
+            padding: 0 8px;
+        }
+    }
+
+    hr {
+        border: none;
+        border-top: 1px solid #ddd;
+        margin: 24px 0;
+    }
 </style>
